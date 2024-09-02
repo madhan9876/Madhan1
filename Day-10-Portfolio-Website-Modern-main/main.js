95ac826db4ebb16e462eaa9136797c110642d6a8
madhan9876 <67366707+madhan9876@users.noreply.github.com>
2024-09-02 22:44:41 +0530

    Delete Day-10-Portfolio-Website-Modern-main directory

--- Day-10-Portfolio-Website-Modern-main/main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings:["Inprocess Quality Assurance", "Dispatch ", "Validation"],
-    typeSpeed : 10,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
-});
